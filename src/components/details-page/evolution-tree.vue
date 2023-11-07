bbc1c92c856078159c67019c68a69f4d1fb50221
almasutami <almas020717@gmail.com>
2023-11-07 17:45:50 +0700

    feat: redirect and loader

--- src/components/details-page/evolution-tree.vue
+++ src/components/details-page/evolution-tree.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 })
 </script>
 <template>
-  <div>
+  <div class="flex flex-row items-center justify-center mt-2">
     <font-awesome-icon
       icon="fa-solid fa-caret-down"
       style="height: 17px; width: 17px; padding: 0; color: #777"
@@ -19,15 +19,18 @@ const props = defineProps({
   </div>
   <div class="flex flex-wrap justify-between">
     <div v-for="(pokemon, i) in props?.evolveTo" :key="i">
-      <div class="py-2 justify-center items-center flex flex-col gap-4">
-        <div class="justify-center items-center flex flex-col">
+      <div>
+        <router-link
+          class="py-2 justify-center items-center flex flex-col"
+          :to="{ name: 'pokemon-details', params: { id: pokemon?.id } }"
+        >
           <img
             :src="pokemon?.sprites?.front_default"
             :alt="pokemon?.name"
             class="h-[100px] w-[100px]"
           />
-          {{ pokemon?.name }}
-        </div>
+          {{ pokemon?.name.slice(0, 1).toUpperCase() + pokemon?.name.slice(1) }}
+        </router-link>
         <evolutionTree
           v-if="pokemon?.evolve_to && pokemon?.evolve_to?.length > 0"
           :evolveTo="pokemon?.evolve_to as PokemonEvolutionTree[]"
--- src/views/pokemon-details.vue
+++ src/views/pokemon-details.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import { usePokemonStore } from '@/stores/pokemon'
 import findFirstTypeColor from '@/utilities/pokemon-type-colors'
 import { storeToRefs } from 'pinia'
-import { onMounted, ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import detailsCard from '@/components/details-card.vue'
 import sadpikachu from '@/assets/sadpikachu.png'
+import pikachu from '@/assets/pikachu.png'
 
 const pokemonStore = usePokemonStore()
 const { selectedPokemon, selectedPokemonSpecies } = storeToRefs(pokemonStore)
 const loading = ref(false)
 const error = ref(false)
 
-const pokemonId = useRoute().params.id as unknown as number
-
-onMounted(async () => {
+const fetchData = async (pokemonId: number) => {
   loading.value = true
   const result = await pokemonStore.fetchPokemonDetails(pokemonId)
   const resultSpecies = await pokemonStore.fetchSpecies(pokemonId)
@@ -25,6 +24,17 @@ onMounted(async () => {
     error.value = true
   }
   loading.value = false
+}
+
+onMounted(() => {
+  const initialPokemonId = useRoute().params.id as unknown as number
+  fetchData(initialPokemonId)
+})
+
+onBeforeRouteUpdate((to, from, next) => {
+  const newPokemonId = to.params.id as unknown as number
+  fetchData(newPokemonId)
+  next()
 })
 </script>
 
@@ -34,7 +44,7 @@ onMounted(async () => {
     :style="{
       backgroundColor: selectedPokemon?.types ? findFirstTypeColor(selectedPokemon?.types) : '#777'
     }"
-    v-if="!error"
+    v-if="!error && !loading"
   >
     <div class="px-4 flex flex-row justify-start my-2 items-center">
       <router-link to="/pokemon">
@@ -45,11 +55,13 @@ onMounted(async () => {
       </router-link>
     </div>
     <detailsCard />
-    <div v-if="loading" class="my-2 flex justify-center">Fetching pokémon...</div>
+  </div>
+  <div v-if="loading" class="flex flex-col justify-center gap-2 h-[100vh] items-center">
+    Fetching pokémon...
+    <img :src="pikachu" alt="pikachu" class="w-[100vw]" />
   </div>
   <div v-if="error" class="flex flex-col justify-center gap-2 h-[100vh] items-center">
     Error in fetching pokémon...
-
     <img :src="sadpikachu" alt="pikachu" class="w-[100vw]" />
   </div>
 </template>
